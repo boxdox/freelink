2184214a87b443d5ec2aa0beab36866b356f2dbd
boxdox <19517445+boxdox@users.noreply.github.com>
2022-01-16 15:15:50 +0530

    added: parcel bundler

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 import { createDirs } from './scripts/createDirs'
 import { Generator } from './scripts/generator'
 import { Validator } from './scripts/validator'
+import { Bundler } from './scripts/bundler'
 
 const main = async () => {
   const dirs = await createDirs()
   const validatedResult = await new Validator(dirs).validate()
-  const generatedPages = await new Generator(
-    validatedResult,
-    dirs
-  ).generatePages()
-  console.log(generatedPages)
+  await new Generator(validatedResult, dirs).generatePages()
+  await new Bundler(dirs).bundle()
 }
 
 main().catch(console.error)
