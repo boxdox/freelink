a9ccc7e6411c885a2173d3713d81c88e1fe95a92
boxdox <19517445+boxdox@users.noreply.github.com>
2022-01-16 15:13:20 +0530

    modified: removed theme configuration from schema

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -9,26 +9,6 @@ export const linkSchema = z.object({
   icon: z.union([z.string(), z.string().url()]).optional(),
 })
 
-const backgroundSchema = z.union([
-  z.object({
-    color: z
-      .string()
-      .min(1, 'color is required if `background image` is not provided'),
-    backgroundImage: z.string().optional(),
-  }),
-  z.object({
-    color: z.string().optional(),
-    backgroundImage: z
-      .string()
-      .min(1, 'background image is required if `color` is not provided'),
-  }),
-])
-
-export const themeSchema = z.object({
-  accentColor: z.string().default('#39e09b'),
-  background: z.union([z.string(), backgroundSchema]),
-})
-
 export const schema = z.object({
   title: z.string({
     invalid_type_error: 'title must be a string',
@@ -38,15 +18,9 @@ export const schema = z.object({
   slug: z.string().optional(),
   main: z.boolean().optional(),
   profilePicture: z.string().optional(),
-  theme: z.union([
-    themeSchema,
-    z.string({
-      required_error: 'theme name is required if theme object is not provided',
-    }),
-  ]),
+  theme: z.string().nullable(),
   links: z.array(linkSchema),
 })
 
 export type ILinkConfig = z.infer<typeof schema>
-export type IThemeOptions = z.infer<typeof themeSchema>
 export type ILink = z.infer<typeof linkSchema>
